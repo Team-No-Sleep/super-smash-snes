7237be99d18b5e7b39958be71e6dc62b3a2b3ff8
kimkablitz <kim.tran549@gmail.com>
2018-10-02 15:28:36 -0700

    sw

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,5 @@
+importScripts('https://www.gstatic.com/firebase/5.1.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebase/5.1.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebase/init.js');
+
+firebase.messaging();
