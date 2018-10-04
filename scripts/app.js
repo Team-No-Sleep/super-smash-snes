5d533a63b9fa85a90ac30987655d3c2238ed23a6
Sweet Ace <lakshmialeman@gmail.com>
2018-10-04 00:42:36 -0700

    pseudocoded game dynamic

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+//set up/initialize global variables for winning/loosing the round (winCount, loseCount, player1BaseHealthPoints, player2BaseHealthPoints, player1CurrentHealthPoints, player2HealthPoints, player1AttackPoints, playerAttackPoints,  player1)
+
+//player 1 attacks player2 the attack happens with attacks points reduces health points by the number of attack points then player2 attacks player 1 the same way therefore attack points reduces health points by the number of attack points.
+
+//update/reduce health bars based on the attacks from player1 and player2
+
+//start timer at 1 minute maybe? to signify/countdown the amount of time a round lasts
+
+//function that resets the game once a round is finished, all the rounds have been played, when a player leaves and declare a forfeit too, when both players leave
+
+//declare a winner of the round when the timer runs out based on who has the highest amount of health points left, or who stands with the highest amount of healthpoints if the timer has not run out yet
+
+//declare a winner of the game if the either player win 3 rounds in a row
