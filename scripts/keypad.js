a2ba7370c1a49ee97f61d55c6308c005d0012b9d
kpcruz <kelvinmembership@gmail.com>
2018-10-04 06:30:13 -0700

    Added separate .png s for the character moves with a new coding process. We can now pump out individual sprites in 10 minutes instead of 90 minues. Fixed every single animation and made sure they worked correspoding to key presses. Made a whole new object for walking with respective methods. They still need some DRY loving. Nuked the /move-sprite and /sprite-animation-P2 folders and moved the needed files into respective folders. Left on collision testing method.

--- scripts/keypad.js
+++ scripts/keypad.js
@@ -52,103 +52,158 @@ var collisionBox = {
 // ==================
 //     GOKU GAMEPAD
 // ==================
-$(document).keyup(function(e){
+$(document).keydown(function(event){
     //comment out this collisionBox object after testing
     collisionBox.checkContact();
     
-    switch(e.which)
+    switch(event.which)
 		{
-        // user presses the "a" PUNCH key
-        case 97:	
-            $('.goku').addClass('punch');
-            $('.goku').addClass('gokuPunch');
+        // user presses the "A" PUNCH key
+        case 65:	
+            $('.goku').addClass('goku-punch');
+            $('.goku').addClass('punch-p1');
             setTimeout(function(e) { 
-                $('.goku').removeClass('punch'); 
-                $('.goku').removeClass('gokuPunch'); 
-            }, 830);
+                $('.goku').removeClass('punch-p1'); 
+                $('.goku').removeClass('goku-punch'); 
+            }, 570);
             break;	
                     
-        // user presses the "s" KICK key
-        case 115:
-            $('.goku').addClass('kick');
-            $('.goku').addClass('gokuKick');
+        // user presses the "S" KICK key
+        case 83:
+            $('.goku').addClass('kick-p1');
+            $('.goku').addClass('goku-kick');
             setTimeout(function(e) { 
-                $('.goku').removeClass('kick'); 
-                $('.goku').removeClass('gokuKick'); 
+                $('.goku').removeClass('kick-p1'); 
+                $('.goku').removeClass('goku-kick'); 
             }, 530);
             break;	
                     
-        // user presses the "d" JUMP key
-        case 100:	
-            $('.goku').addClass('jump');
-            $('.goku').addClass('gokuJump');
+        // user presses the "D" JUMP key
+        case 68:	
+            $('.goku').addClass('jump-p1');
+            $('.goku').addClass('goku-jump');
             setTimeout(function(e) { 
-                $('.goku').removeClass('jump'); 
-                $('.goku').removeClass('gokuJump'); 
-            }, 1530);
+                $('.goku').removeClass('jump-p1'); 
+                $('.goku').removeClass('goku-jump'); 
+            }, 830);
             break;	
-                    
-         // user presses the "q" WALK LEFT key 
-         case 113:	
-             $('.goku').css({"margin-left": "-=10px" });;
-         break;
-
-         // user presses the "e"  WALK RIGHT key
-         case 101:	
-             $('.goku').css({"margin-left": "+=10px" });
-         break;
-     }
+         }
 });
-
-
 
 // ==================
 //     RYU GAMEPAD
 // ==================
-$(document).keypress(function(e){
+$(window).keydown(function(event){
     //comment out this collisionBox object after testing
     collisionBox.checkContact();
 
-    switch(e.which)
+    switch(event.which)
 		{
         // user presses the "j" PUNCH key
-        case 106:	
+        case 74:	
             $('.ryu').addClass('ryu-punch');
             $('.ryu').addClass('punch-p2');
-            setTimeout(function(e) { 
+            setTimeout(function(event) { 
                 $('.ryu').removeClass('punch-p2'); 
                 $('.ryu').removeClass('ryu-punch'); 
             }, 230);
             break;	
                     
         // user presses the "k" KICK key
-        case 107:
+        case 75:
             $('.ryu').addClass('ryu-kick');
             $('.ryu').addClass('kick-p2');
-            setTimeout(function(e) { 
+            setTimeout(function(event) { 
                 $('.ryu').removeClass('kick-p2'); 
                 $('.ryu').removeClass('ryu-kick'); 
-            }, 540);
+            }, 940);
             break;	
                     
         // user presses the "l" JUMP key
-        case 108:	
+        case 76:
             $('.ryu').addClass('ryu-jump');
             $('.ryu').addClass('jump-p2');
-            setTimeout(function(e) { 
+            setTimeout(function(event) { 
                 $('.ryu').removeClass('jump-p2'); 
                 $('.ryu').removeClass('ryu-jump'); 
             }, 870);
             break;	
-                    
-        // user presses the "u" WALK LEFT key
-        case 117:	
-            $('.ryu').css({"margin-left": "-=10px" });
-            break;
-            
-        // user presses the "o" WALK RIGHT key 
-        case 111:	
-            $('.ryu').css({"margin-left": "+=10px"});
-            break;
         }
 });
+
+// =============
+//    WALKING
+// =============
+
+//GOKUWALK
+var gokuWalk = {
+    player: $(".goku"),
+    speed: 0.5,
+    direction: {
+        left: false,
+        right: false,
+    },
+    keyDown: $(document).keydown(function(event) {
+        var keycode = event.keyCode;
+        event.preventDefault();
+        if (keycode === 81) gokuWalk.direction.left = true;
+        if (keycode === 69) gokuWalk.direction.right = true;
+    }),
+    keyUp: $(document).keyup(function(event) {
+        var keycode = event.keyCode;
+        event.preventDefault();
+        if (keycode === 81) gokuWalk.direction.left = false;
+        if (keycode === 69) gokuWalk.direction.right = false;
+    }), 
+    animateGo: $(document).keydown(function() {
+        if (gokuWalk.direction.left === true || gokuWalk.direction.right === true) {
+            gokuWalk.player.addClass('goku-walk walk-p1');
+        } 
+    }),
+    animateStop: $(document).keyup(function() {
+        if (gokuWalk.direction.left === false || gokuWalk.direction.right === false) {
+            gokuWalk.player.removeClass('goku-walk walk-p1');
+        };
+    }),
+    move: function() {
+        if (gokuWalk.direction.left) gokuWalk.player.css("left", (gokuWalk.player.position().left - gokuWalk.speed) + "px");
+        if (gokuWalk.direction.right) gokuWalk.player.css("left", (gokuWalk.player.position().left + gokuWalk.speed) + "px");
+    },
+};
+setInterval(gokuWalk.move, 1);
+
+//RYUWALK
+var ryuWalk = {
+    player: $(".ryu"),
+    speed: 0.5,
+    direction: {
+        left: false,
+        right: false,
+    },
+    keyDown: $(document).keydown(function(event) {
+        var keycode = event.keyCode;
+        event.preventDefault();
+        if (keycode === 85) ryuWalk.direction.left = true;
+        if (keycode === 79) ryuWalk.direction.right = true;
+    }), 
+    keyUp: $(document).keyup(function(event) {
+        var keycode = event.keyCode;
+        event.preventDefault();
+        if (keycode === 85) ryuWalk.direction.left = false;
+        if (keycode === 79) ryuWalk.direction.right = false;
+    }),
+    animateGo: $(document).keydown(function() {
+        if (ryuWalk.direction.left === true || ryuWalk.direction.right === true) {
+            ryuWalk.player.addClass('ryu-walk walk-p1');
+        } 
+    }),
+    animateStop: $(document).keyup(function() {
+        if (ryuWalk.direction.left === false || ryuWalk.direction.right === false) {
+            ryuWalk.player.removeClass('ryu-walk walk-p1');
+        };
+    }),
+    move: function() {
+        if (ryuWalk.direction.left) ryuWalk.player.css("left", (ryuWalk.player.position().left - ryuWalk.speed) + "px");
+        if (ryuWalk.direction.right) ryuWalk.player.css("left", (ryuWalk.player.position().left + ryuWalk.speed) + "px");
+    },
+};setInterval(ryuWalk.move, 1);
